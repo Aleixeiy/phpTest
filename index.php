cafc7d3a12717d202a936e4ae059ec495dc991c0
Lesha <Pleskach.Lesha@yandex.ru>
2021-11-29 23:08:28 +0300

    test

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "123456789101112131415161718192021222324252627282930";
+        ?>
+    </body>
+</html>
